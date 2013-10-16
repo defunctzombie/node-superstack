1c2c6a3dbafaa0abee05fc5fe1dd2ed22bcff8ba
Roman Shtylman <shtylman@gmail.com>
2013-10-16 14:29:24 -0400

    add many listeners to perf test

--- perf.js
+++ perf.js
@@ -9,19 +9,24 @@ var foobar = function() {
     count++;
 };
 
-var ev = new EventEmitter();
-ev.on('foo', foobar);
-
-var max = 1000000;
-
 var start = process.hrtime();
+
+var ev = new EventEmitter();
+ev.setMaxListeners(0);
+
+var io = 1000;
+for (var i=0 ; i<io ; ++i) {
+    ev.on('foo', foobar);
+}
+
+var max = 100000;
 
 for (var i=0 ; i<max ; ++i) {
     ev.emit('foo');
 }
 
 var diff = process.hrtime(start);
-assert(count === max);
+assert(count === max * io);
 
 console.log('benchmark took %d seconds', (diff[0] * 1e9 + diff[1]) / 1e9);
 console.log(process.memoryUsage());
